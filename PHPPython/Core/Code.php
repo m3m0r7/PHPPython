5212580c2a8f9417931cd42b4b06c066a98f48a4
memory-agape <memory.style.serv@gmail.com>
2016-06-12 00:28:12 +0900

    add marshal types

--- PHPPython/Core/Code.php
+++ PHPPython/Core/Code.php
@@ -41,10 +41,78 @@ class Code {
     private function _disAssemble () {
         $binaryReader = new Utility\BinaryReader($this->_codeHandle);
         $this->_marshalType = $binaryReader->readByte();
-        $this->_argumentCount = $binaryReader->readLong();
-        $this->_localCount = $binaryReader->readLong();
-        $this->_stackSize = $binaryReader->readLong();
-        $this->_flags = $binaryReader->readLong();
+        switch ($this->_marshalType) {
+            case 'c':
+                $this->_argumentCount = $binaryReader->readLong();
+                $this->_localCount = $binaryReader->readLong();
+                $this->_stackSize = $binaryReader->readLong();
+                $this->_flags = $binaryReader->readLong();
+                return;
+            case '.':
+                // coming soon...
+                return;
+            case '0':
+                // coming soon...
+                return;
+            case 'N':
+                // coming soon...
+                return;
+            case 'T':
+                // coming soon...
+                return;
+            case 'F':
+                // coming soon...
+                return;
+            case 'S':
+                // coming soon...
+                return;
+            case 'f':
+                // coming soon...
+                return;
+            case 'g':
+                // coming soon...
+                return;
+            case 'i':
+                // coming soon...
+                return;
+            case 'I':
+                // coming soon...
+                return;
+            case 'x':
+                // coming soon...
+                return;
+            case 'y':
+                // coming soon...
+                return;
+            case 'l':
+                // coming soon...
+                return;
+            case 'R':
+                // coming soon...
+                return;
+            case 's':
+                // coming soon...
+                return;
+            case 't':
+                // coming soon...
+                return;
+            case 'u':
+                // coming soon...
+                return;
+            case '(':
+                // coming soon...
+                return;
+            case '[':
+                // coming soon...
+                return;
+            case '{':
+                // coming soon...
+                return;
+            case '<':
+            case '>':
+                // coming soon...
+                return;
+        }
     }
 
 }
